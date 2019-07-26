31a126b956c3826c6dc76c22c614362cceeb4fac
Mohan Kumar <MohanKumarYum@Gmail.com>
2019-07-26 22:59:22 +0000

    Streamlined the single build option.

--- how_to_tabs/jake.cmd
+++ how_to_tabs/jake.cmd
@@ -2,4 +2,11 @@
 
 REM Suppress commands displayed on the console.
 
-node_modules\.bin\jake %*
+if not exist node_modules\.bin\jake (
+    
+echo Building npm modules:
+call npm rebuild
+
+)
+ 
+call node_modules\.bin\jake %*
